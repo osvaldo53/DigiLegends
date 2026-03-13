91b5dd727912cdfcccb00e38f33c2b970822eeb7
Osvaldo Souza <96638932+osvaldo53@users.noreply.github.com>
2026-03-13 00:54:01 -0300

    digidex

---
+++ data/digimons.js
@@ -0,0 +1,228 @@
+const digimons = {
+  "Agumon": {
+    "stage": "Rookie",
+    "type": "Vaccine",
+    "attribute": "Fire",
+    "stats": { "hp": 450, "sp": 20, "atk": 68, "def": 45, "int": 15, "spd": 37 },
+    "evolution": [
+      { "digimon": "Greymon", "requirements": { "level": 15, "atk": 90 } }
+    ]
+  },
+
+  "Greymon": {
+    "stage": "Champion",
+    "type": "Vaccine",
+    "attribute": "Fire",
+    "stats": { "hp": 850, "sp": 40, "atk": 120, "def": 80, "int": 30, "spd": 55 },
+    "evolution": [
+      { "digimon": "MetalGreymon", "requirements": { "level": 30, "atk": 150, "def": 100 } }
+    ]
+  },
+
+  "MetalGreymon": {
+    "stage": "Ultimate",
+    "type": "Vaccine",
+    "attribute": "Fire",
+    "stats": { "hp": 1200, "sp": 55, "atk": 170, "def": 120, "int": 45, "spd": 65 },
+    "evolution": [
+      { "digimon": "WarGreymon", "requirements": { "level": 50, "atk": 200, "spd": 120 } }
+    ]
+  },
+
+  "WarGreymon": {
+    "stage": "Mega",
+    "type": "Vaccine",
+    "attribute": "Fire",
+    "stats": { "hp": 1600, "sp": 70, "atk": 230, "def": 160, "int": 70, "spd": 95 },
+    "evolution": []
+  },
+
+  "Gabumon": {
+    "stage": "Rookie",
+    "type": "Data",
+    "attribute": "Fire",
+    "stats": { "hp": 420, "sp": 25, "atk": 65, "def": 40, "int": 20, "spd": 35 },
+    "evolution": [
+      { "digimon": "Garurumon", "requirements": { "level": 15, "spd": 50 } }
+    ]
+  },
+
+  "Garurumon": {
+    "stage": "Champion",
+    "type": "Data",
+    "attribute": "Fire",
+    "stats": { "hp": 800, "sp": 38, "atk": 115, "def": 75, "int": 35, "spd": 60 },
+    "evolution": [
+      { "digimon": "WereGarurumon", "requirements": { "level": 30, "atk": 140, "spd": 90 } }
+    ]
+  },
+
+  "WereGarurumon": {
+    "stage": "Ultimate",
+    "type": "Data",
+    "attribute": "Fire",
+    "stats": { "hp": 1150, "sp": 50, "atk": 165, "def": 110, "int": 50, "spd": 85 },
+    "evolution": [
+      { "digimon": "MetalGarurumon", "requirements": { "level": 50, "atk": 200, "spd": 120 } }
+    ]
+  },
+
+  "MetalGarurumon": {
+    "stage": "Mega",
+    "type": "Data",
+    "attribute": "Fire",
+    "stats": { "hp": 1550, "sp": 68, "atk": 225, "def": 150, "int": 75, "spd": 100 },
+    "evolution": []
+  },
+
+  "Patamon": {
+    "stage": "Rookie",
+    "type": "Free",
+    "attribute": "Wind",
+    "stats": { "hp": 400, "sp": 22, "atk": 60, "def": 35, "int": 25, "spd": 40 },
+    "evolution": [
+      { "digimon": "Angemon", "requirements": { "level": 16, "int": 40 } }
+    ]
+  },
+
+  "Angemon": {
+    "stage": "Champion",
+    "type": "Vaccine",
+    "attribute": "Light",
+    "stats": { "hp": 780, "sp": 36, "atk": 110, "def": 70, "int": 60, "spd": 55 },
+    "evolution": [
+      { "digimon": "MagnaAngemon", "requirements": { "level": 30, "int": 100 } }
+    ]
+  },
+
+  "MagnaAngemon": {
+    "stage": "Ultimate",
+    "type": "Vaccine",
+    "attribute": "Light",
+    "stats": { "hp": 1150, "sp": 50, "atk": 150, "def": 110, "int": 120, "spd": 75 },
+    "evolution": [
+      { "digimon": "Seraphimon", "requirements": { "level": 50, "int": 150 } }
+    ]
+  },
+
+  "Seraphimon": {
+    "stage": "Mega",
+    "type": "Vaccine",
+    "attribute": "Light",
+    "stats": { "hp": 1600, "sp": 70, "atk": 200, "def": 150, "int": 180, "spd": 90 },
+    "evolution": []
+  },
+
+  "Tentomon": {
+    "stage": "Rookie",
+    "type": "Vaccine",
+    "attribute": "Electric",
+    "stats": { "hp": 430, "sp": 23, "atk": 62, "def": 42, "int": 18, "spd": 36 },
+    "evolution": [
+      { "digimon": "Kabuterimon", "requirements": { "level": 15, "def": 55 } }
+    ]
+  },
+
+  "Kabuterimon": {
+    "stage": "Champion",
+    "type": "Vaccine",
+    "attribute": "Electric",
+    "stats": { "hp": 820, "sp": 38, "atk": 115, "def": 85, "int": 40, "spd": 50 },
+    "evolution": [
+      { "digimon": "MegaKabuterimon", "requirements": { "level": 30, "def": 120 } }
+    ]
+  },
+
+  "MegaKabuterimon": {
+    "stage": "Ultimate",
+    "type": "Vaccine",
+    "attribute": "Electric",
+    "stats": { "hp": 1200, "sp": 52, "atk": 160, "def": 150, "int": 60, "spd": 70 },
+    "evolution": [
+      { "digimon": "HerculesKabuterimon", "requirements": { "level": 50, "def": 180 } }
+    ]
+  },
+
+  "HerculesKabuterimon": {
+    "stage": "Mega",
+    "type": "Vaccine",
+    "attribute": "Electric",
+    "stats": { "hp": 1650, "sp": 72, "atk": 210, "def": 200, "int": 80, "spd": 85 },
+    "evolution": []
+  },
+
+  "Biyomon": {
+    "stage": "Rookie",
+    "type": "Vaccine",
+    "attribute": "Wind",
+    "stats": { "hp": 410, "sp": 24, "atk": 58, "def": 38, "int": 22, "spd": 39 },
+    "evolution": [
+      { "digimon": "Birdramon", "requirements": { "level": 15, "spd": 50 } }
+    ]
+  },
+
+  "Birdramon": {
+    "stage": "Champion",
+    "type": "Vaccine",
+    "attribute": "Fire",
+    "stats": { "hp": 800, "sp": 36, "atk": 110, "def": 70, "int": 40, "spd": 55 },
+    "evolution": [
+      { "digimon": "Garudamon", "requirements": { "level": 30, "atk": 140, "spd": 80 } }
+    ]
+  },
+  "Garudamon": {
+    "stage": "Ultimate",
+    "type": "Vaccine",
+    "attribute": "Fire",
+    "stats": { "hp": 1150, "sp": 50, "atk": 160, "def": 110, "int": 60, "spd": 85 },
+    "evolution": [
+      { "digimon": "Phoenixmon", "requirements": { "level": 50, "atk": 200, "spd": 120 } }
+    ]
+  },
+
+  "Phoenixmon": {
+    "stage": "Mega",
+    "type": "Vaccine",
+    "attribute": "Fire",
+    "stats": { "hp": 1600, "sp": 70, "atk": 220, "def": 150, "int": 90, "spd": 100 },
+    "evolution": []
+  },
+
+  "Palmon": {
+    "stage": "Rookie",
+    "type": "Data",
+    "attribute": "Plant",
+    "stats": { "hp": 440, "sp": 21, "atk": 63, "def": 41, "int": 19, "spd": 34 },
+    "evolution": [
+      { "digimon": "Togemon", "requirements": { "level": 15, "def": 50 } }
+    ]
+  },
+
+  "Togemon": {
+    "stage": "Champion",
+    "type": "Data",
+    "attribute": "Plant",
+    "stats": { "hp": 820, "sp": 35, "atk": 110, "def": 85, "int": 40, "spd": 45 },
+    "evolution": [
+      { "digimon": "Lillymon", "requirements": { "level": 30, "spd": 70, "int": 80 } }
+    ]
+  },
+
+  "Lillymon": {
+    "stage": "Ultimate",
+    "type": "Data",
+    "attribute": "Plant",
+    "stats": { "hp": 1150, "sp": 50, "atk": 150, "def": 100, "int": 120, "spd": 85 },
+    "evolution": [
+      { "digimon": "Rosemon", "requirements": { "level": 50, "int": 150, "spd": 110 } }
+    ]
+  },
+
+  "Rosemon": {
+    "stage": "Mega",
+    "type": "Data",
+    "attribute": "Plant",
+    "stats": { "hp": 1600, "sp": 70, "atk": 210, "def": 140, "int": 170, "spd": 95 },
+    "evolution": []
+  }
+}
